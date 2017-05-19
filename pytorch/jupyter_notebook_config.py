d948e23723aba447eed119b47388a30d6b226ca4
Naren Dasan <naren@narendasan.com>
2017-05-19 16:09:33 -0700

    Update jupyter_notebook_config.py

--- pytorch/jupyter_notebook_config.py
+++ pytorch/jupyter_notebook_config.py
@@ -5,8 +5,3 @@ c.NotebookApp.ip = '*'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
 c.MultiKernelManager.default_kernel_name = 'python2'
-
-# sets a password if PASSWORD is set in the environment
-if 'PASSWORD' in os.environ:
-  c.NotebookApp.password = passwd(os.environ['PASSWORD'])
-del os.environ['PASSWORD']
